7c4604a28fabf49e02b4eeb37d5f8fcee0396ca9
Kevin Wittmer <kevinwittmer@outlook.de>
2024-03-14 08:31:16 +0100

    MSP-18 Navbar erstellt und angefangen die Kleidung hinzufügen card zu designen

--- src/app/components/app-input/app-input.component.ts
+++ src/app/components/app-input/app-input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-app-input',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app-input.component.css']
 })
 export class AppInputComponent {
+@Input() placeholder: string = "Default Placeholder";
+
 
 }
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -4,24 +4,27 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { AddItemViewComponent } from './add-item-view/add-item-view.component';
 import { AppInputComponent } from './app-input/app-input.component';
 import { AppDropDownComponent } from './app-drop-down/app-drop-down.component';
-import { ImageComponent } from './image/image.component';
 import { AppButtonComponent } from './app-button/app-button.component';
 import { AppImageViewerComponent } from './app-image-viewer/app-image-viewer.component';
 
 
 
 @NgModule({
-  declarations: [
-    NavBarComponent,
-    AddItemViewComponent,
-    AppInputComponent,
+    declarations: [
+        NavBarComponent,
+        AddItemViewComponent,
+        AppInputComponent,
+        AppDropDownComponent,
+        AppButtonComponent,
+        AppImageViewerComponent
+    ],
+  exports: [
     AppDropDownComponent,
-    ImageComponent,
-    AppButtonComponent,
-    AppImageViewerComponent
+    NavBarComponent,
+    AddItemViewComponent
   ],
-  imports: [
-    CommonModule
-  ]
+    imports: [
+        CommonModule
+    ]
 })
 export class ComponentsModule { }
