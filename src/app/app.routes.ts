7b1f21c8888472d9bd7582873fd975d3f86231e6
reinhold <reinhold6@schule.bremen.de>
2024-05-02 11:37:21 +0200

    MSP-29 Unnötiege Imports entfernt

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { AppComponent } from './app.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, canActivate: [] },
