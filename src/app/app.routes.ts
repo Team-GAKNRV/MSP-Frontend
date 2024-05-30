6f131deafb2c74180276b2eec607044d0ef7981a
Zanatros <mineplayslpemail@gmail.com>
2024-05-30 18:59:41 +0200

    MSP-54 Added Keycloak

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import {AuthGuard} from "./guards/auth.guard";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent, canActivate: [AuthGuard] }
+];
 
