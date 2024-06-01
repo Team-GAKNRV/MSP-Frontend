5c125128c3d0e16eff196355be30d65c637dd038
topgggggg <156314616+topgggggg@users.noreply.github.com>
2024-06-01 20:33:36 +0200

    MSP-56 init custom type for clothing items

---
+++ src/app/classes/clothing-item.class.ts
@@ -0,0 +1,25 @@
+export class ClothingItem {
+    public name: string;
+    public image: string;
+    public brand: string;
+    public color: string;
+    public masterCategory: string;
+    public subCategory: string;
+    public type: string;
+    public season: string;
+    public usage: string;
+    private isFavorite: boolean;
+
+    constructor(name: string, image: string, brand: string, color: string, masterCategory: string, subCategory: string, type: string, season: string, usage: string, isFavorite: boolean) {
+        this.name = name;
+        this.image = image;
+        this.brand = brand;
+        this.color = color;
+        this.masterCategory = masterCategory;
+        this.subCategory = subCategory;
+        this.type = type;
+        this.season = season;
+        this.usage = usage;
+        this.isFavorite = isFavorite;
+    }
+}
