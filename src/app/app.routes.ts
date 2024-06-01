45762257ffa8913f8f8780d58ab905e5cce8ad10
topgggggg <156314616+topgggggg@users.noreply.github.com>
2024-06-01 20:29:06 +0200

    MSP-56 add route for the clothing view component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
+import { ClosetComponent } from './components/closet/closet.component';
+import { ClothingViewComponent } from './components/clothing-view/clothing-view.component';
 import { HomeComponent } from "./components/home/home.component";
 import { AuthGuard } from "./guards/auth.guard";
-import { ClosetComponent } from './components/closet/closet.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, canActivate: [AuthGuard] },
-  { path: 'closet', component: ClosetComponent }
+  { path: 'closet', component: ClosetComponent },
+  { path: 'view', component: ClothingViewComponent },
 ];
 
