d81782dd4e902d7f1d297a21eba9385ea86e7a8c
topgggggg <156314616+topgggggg@users.noreply.github.com>
2024-06-01 20:38:29 +0200

    MSP-56 extend SearchOption type

--- src/app/types/search-option.type.ts
+++ src/app/types/search-option.type.ts
@@ -1,3 +1,8 @@
-import { BaseColour } from "../enums/baseColours.enum";
+import { ArticleType } from "../enums/articleType.enum";
+import { BaseColour } from "../enums/baseColour.enum";
+import { MasterCategory } from "../enums/masterCategory.enum";
+import { Season } from "../enums/season.enum";
+import { SubCategory } from "../enums/subCategory.enum";
+import { Usage } from "../enums/usage.enum";
 
-export type SearchOption = typeof BaseColour | null;
+export type SearchOption = typeof ArticleType | typeof BaseColour | typeof MasterCategory | typeof Season | typeof SubCategory | typeof Usage | null;
