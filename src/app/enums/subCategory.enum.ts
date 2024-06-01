38e7c9124a97281ed29c03df958f75754e4a7859
topgggggg <156314616+topgggggg@users.noreply.github.com>
2024-06-01 20:31:06 +0200

    MSP-56 add clothing labels as enums

---
+++ src/app/enums/subCategory.enum.ts
@@ -0,0 +1,13 @@
+export enum SubCategory {
+    Bottomwear,
+    Topwear,
+    Dress,
+    Innerwear,
+    Loungewear_And_Nightwear,
+    Headwear,
+    Scarves,
+    Socks,
+    Shoes,
+    Sandal,
+    Flip_Flops
+}
