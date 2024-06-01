45d25856d15a1e80b28f85a2ef2ac44a0e330c5d
topgggggg <156314616+topgggggg@users.noreply.github.com>
2024-06-01 20:34:02 +0200

    MSP-56 init add item card

---
+++ src/app/components/add-item-card/add-item-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-item-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-item-card.component.html',
+  styleUrl: './add-item-card.component.css'
+})
+export class AddItemCardComponent {
+
+}
