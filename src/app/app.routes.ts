7ffb6b15fe5ff55b5ae1fb5f8c9d95044087a533
Zanatros <mineplayslpemail@gmail.com>
2024-06-02 15:13:14 +0200

    MSP-61 Added new route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from "./components/home/home.component";
 import { AuthGuard } from "./guards/auth.guard";
 import { ClosetComponent } from './components/closet/closet.component';
+import {InspirationsComponent} from "./components/inspirations/inspirations.component";
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, canActivate: [AuthGuard] },
-  { path: 'closet', component: ClosetComponent }
+  { path: 'closet', component: ClosetComponent },
+  { path: 'inspirations', component: InspirationsComponent }
 ];
 
