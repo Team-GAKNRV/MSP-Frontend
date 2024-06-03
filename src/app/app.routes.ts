2512ece891c604b9f39519cf78a43fd96b711d48
topgggggg <156314616+topgggggg@users.noreply.github.com>
2024-06-03 22:20:57 +0200

    MSP-63 remove route for clothing view as its replaced by clothing modal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,13 @@
 import { Routes } from '@angular/router';
 import { ClosetComponent } from './components/closet/closet.component';
-import { ClothingViewComponent } from './components/clothing-view/clothing-view.component';
 import { HomeComponent } from "./components/home/home.component";
-import { AuthGuard } from "./guards/auth.guard";
 import { OutfitsComponent } from "./components/outfits/outfits.component";
+import { AuthGuard } from "./guards/auth.guard";
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'closet', component: ClosetComponent },
   { path: 'outfits', component: OutfitsComponent },
   { path: 'closet', component: ClosetComponent },
-  { path: 'view', component: ClothingViewComponent }
 ];
 
