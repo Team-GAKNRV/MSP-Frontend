c625fa26eaeb6cd21aa9926aa2c6562f67ffe470
topgggggg <156314616+topgggggg@users.noreply.github.com>
2024-06-04 21:54:31 +0200

    MSP-63 add file subject to modal data service

--- src/app/services/modal-data.service.ts
+++ src/app/services/modal-data.service.ts
@@ -6,9 +6,15 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class ModalDataService {
     private dataSubject = new BehaviorSubject<any>(null);
+    private fileSubject = new BehaviorSubject<any>(null);
     data$ = this.dataSubject.asObservable();
+    file$ = this.fileSubject.asObservable();
 
     setData(data: any) {
         this.dataSubject.next(data);
+    }
+
+    setFile(file: any) {
+        this.fileSubject.next(file);
     }
 }
