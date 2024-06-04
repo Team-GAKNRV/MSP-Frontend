8a036d65abf4dc1211f9acc24bd0f5616f74b21a
reinhold <r.honstein@hotmail.de>
2024-06-04 18:14:51 +0200

    Anzeige der Kleidung

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,42 @@
+import { ViewChild } from '@angular/core';
+import { ClothingItemCardComponent } from '../clothing-item-card/clothing-item-card.component';
 import { Component } from '@angular/core';
+import { KeycloakService } from 'keycloak-angular';
+import { NgForOf } from '@angular/common';
+
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css',
+    imports: [NgForOf, ClothingItemCardComponent]
 })
-export class HomeComponent { }
+export class HomeComponent {
+  @ViewChild(ClothingItemCardComponent) child: any;
+  numberOfCards = [];
+
+  constructor(private keycloakService: KeycloakService) { }
+
+  async getAllClothingItems(): Promise<void> {
+    const apiUrl = `http://localhost:8080/api/v1/user/clothing-items?user-id=${this.keycloakService.getKeycloakInstance().tokenParsed?.sub}`;
+    const response = await fetch(apiUrl, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${await this.keycloakService.getToken()}`,
+        'Content-Type': 'application/json'
+      }
+    });
+
+    if (response.ok) {
+      this.numberOfCards = await response.json();
+    } else {
+      console.error(response.status);
+    }
+  }
+  
+
+  ngOnInit(): void {
+    this.getAllClothingItems();
+  }
+ }
