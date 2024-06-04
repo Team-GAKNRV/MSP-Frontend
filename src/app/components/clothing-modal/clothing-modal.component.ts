3668518fbe5aab226fa29b834ab2c9fc14f28c8a
topgggggg <156314616+topgggggg@users.noreply.github.com>
2024-06-04 21:59:03 +0200

    MSP-63 implement saving logic to clothing modal

--- src/app/components/clothing-modal/clothing-modal.component.ts
+++ src/app/components/clothing-modal/clothing-modal.component.ts
@@ -1,11 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { KeycloakService } from 'keycloak-angular';
+import { ClothingItem } from '../../classes/clothing-item.class';
 import { ArticleType } from '../../enums/articleType.enum';
 import { BaseColour } from '../../enums/baseColour.enum';
 import { MasterCategory } from '../../enums/masterCategory.enum';
 import { Season } from '../../enums/season.enum';
 import { SubCategory } from '../../enums/subCategory.enum';
 import { Usage } from '../../enums/usage.enum';
+import { ClothingImageConverter } from '../../services/clothing-image-converter.service';
+import { ClothingService } from '../../services/clothing.service';
 import { ModalDataService } from '../../services/modal-data.service';
 import { InputComponent } from "../input/input.component";
 
@@ -41,7 +45,7 @@ export class ClothingModalComponent implements OnInit {
   subCategory = SubCategory;
   usage = Usage;
 
-  constructor(private modalDataService: ModalDataService) { }
+  constructor(private clothingService: ClothingService, private clothingImageConverterService: ClothingImageConverter, private keycloakService: KeycloakService, private modalDataService: ModalDataService) { }
 
   ngOnInit() {
     this.modalDataService.data$.subscribe(clothingItemData => {
@@ -70,13 +74,39 @@ export class ClothingModalComponent implements OnInit {
     this.usageValue = this.clothingItem.usage.replaceAll(' ', '_');
   }
 
-  handleSaveClick() {
-    // Logic to save
-    this.close.emit();
+  convertClothingImageToBase64() {
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.clothingItem.image = reader.result;
+    };
+    reader.readAsDataURL(this.clothingImage);
+  }
+
+  async handleSaveClick() {
+    const bearerToken = await this.keycloakService.getToken();
+
+    if (this.saveAsNewClothingItem) {
+      const userId = this.keycloakService.getKeycloakInstance().tokenParsed?.sub;
+
+      if (userId !== undefined) {
+        const base64Image = this.clothingImageConverterService.stripDataUrlPrefix(this.clothingItem.image);
+        const clothingItemToUpload = new ClothingItem(this.nameValue, base64Image, this.brandValue, this.baseColourValue, this.masterCategoryValue, this.subCategoryValue, this.articleTypeValue, this.seasonValue, this.usageValue, this.clothingItem.isFavorite);
+        const filledInClothingItem = this.clothingService.createStaticClothingInformation(clothingItemToUpload);
+
+        await this.clothingService.addClothingItem(bearerToken, userId, filledInClothingItem).then(() => this.close.emit());
+      }
+
+    } else {
+      const base64Image = this.clothingImageConverterService.stripDataUrlPrefix(this.clothingItem.image);
+      let clothingItemToUpload = new ClothingItem(this.nameValue, base64Image, this.brandValue, this.baseColourValue, this.masterCategoryValue, this.subCategoryValue, this.articleTypeValue, this.seasonValue, this.usageValue, this.clothingItem.isFavorite);
+
+      await this.clothingService.updateClothingItem(bearerToken, this.clothingItem._id, clothingItemToUpload).then(() => this.close.emit());
+    }
+
+    this.modalDataService.setNeedsReload(true);
   }
 
   handleCancelClick() {
-    // Logic to cancel
     this.close.emit();
   }
 
