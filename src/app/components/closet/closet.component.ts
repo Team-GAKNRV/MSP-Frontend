0e22190c12a42696bf1085b1bd69b9ede77b3240
topgggggg <156314616+topgggggg@users.noreply.github.com>
2024-06-04 22:00:20 +0200

    MSP-63 implement needs reload and save as new item logic

--- src/app/components/closet/closet.component.ts
+++ src/app/components/closet/closet.component.ts
@@ -1,8 +1,13 @@
 import { NgForOf, NgIf } from '@angular/common';
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 import { KeycloakService } from "keycloak-angular";
+import { ClothingItem } from '../../classes/clothing-item.class';
 import { ClothingImageConverter } from '../../services/clothing-image-converter.service';
+import { ClothingService } from '../../services/clothing.service';
+import { FileUploadService } from '../../services/file-upload.service';
 import { ModalDataService } from '../../services/modal-data.service';
+import { ClassifiedClothingItem } from '../../types/classified-clothing-item.type';
 import { AddItemCardComponent } from "../add-item-card/add-item-card.component";
 import { ClothingItemCardComponent } from "../clothing-item-card/clothing-item-card.component";
 import { ClothingModalComponent } from "../clothing-modal/clothing-modal.component";
@@ -28,11 +33,13 @@ export class ClosetComponent implements OnInit {
   @ViewChild('fileInput') fileInput: ElementRef | undefined;
 
   showModal: boolean = false;
+  saveAsNewItem: boolean = false;
+  needsReload: boolean = false;
   selectedCard: any = false;
   selectedFile: File | null = null;
   numberOfCards = [];
 
-  constructor(private keycloakService: KeycloakService, private modalDataService: ModalDataService, private clothingImageConverterService: ClothingImageConverter) { }
+  constructor(private keycloakService: KeycloakService, private modalDataService: ModalDataService, private clothingService: ClothingService, private clothingImageConverterService: ClothingImageConverter, private fileUploadService: FileUploadService, private router: Router) { }
 
   async getAllClothingItems(): Promise<void> {
     const apiUrl = `http://localhost:8080/api/v1/user/clothing-items?user-id=${this.keycloakService.getKeycloakInstance().tokenParsed?.sub}`;
@@ -55,6 +62,14 @@ export class ClosetComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.modalDataService.needsReload$.subscribe(needsReload => {
+      this.needsReload = needsReload;
+      if (this.needsReload) {
+        this.getAllClothingItems();
+        this.modalDataService.setNeedsReload(false);
+      }
+    });
+
     this.getAllClothingItems();
   }
 
@@ -74,16 +89,20 @@ export class ClosetComponent implements OnInit {
 
   closeModal() {
     this.showModal = false;
+    this.saveAsNewItem = false;
     this.selectedCard = null;
   }
 
   classifyImageAndOpenClothingView() {
     if (this.selectedFile !== null) {
       this.clothingService.classifyImage(this.selectedFile).subscribe(
-        (response: any) => {
-          this.fileUploadService.setFile(this.selectedFile!);
-          this.clothingService.setClassificationInformation(response);
-          this.router.navigateByUrl('view');
+        (classificationInformation: ClassifiedClothingItem) => {
+          const classifiedClothingItem = new ClothingItem('', '', '', classificationInformation.baseColour, classificationInformation.masterCategory, classificationInformation.subCategory, classificationInformation.articleType, classificationInformation.season, classificationInformation.usage, false);
+
+          this.modalDataService.setFile(this.selectedFile);
+          this.modalDataService.setData(classifiedClothingItem);
+          this.saveAsNewItem = true;
+          this.showModal = true;
         },
         (error: any) => console.error('Upload failed', error)
       );
