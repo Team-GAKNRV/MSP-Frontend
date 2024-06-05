9fced5cca5890d90233d2c55e43be99ec4bc1c7a
topgggggg <156314616+topgggggg@users.noreply.github.com>
2024-06-05 10:38:03 +0200

    MSP-61 make add button invisble if added

--- src/app/components/inspiration-card/inspiration-card.component.ts
+++ src/app/components/inspiration-card/inspiration-card.component.ts
@@ -1,31 +1,35 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
-import {KeycloakService} from "keycloak-angular";
-import {Router} from "@angular/router";
-import {ClothingImageConverter} from "../../services/clothing-image-converter.service";
+import { Component, Input, OnInit } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { KeycloakService } from 'keycloak-angular';
+import { Router } from '@angular/router';
+import { ClothingImageConverter } from '../../services/clothing-image-converter.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-inspiration-card',
   standalone: true,
-  imports: [
-    FontAwesomeModule
-  ],
+  imports: [FontAwesomeModule, NgIf],
   templateUrl: './inspiration-card.component.html',
-  styleUrl: './inspiration-card.component.css'
+  styleUrl: './inspiration-card.component.css',
 })
 export class InspirationCardComponent {
-
   @Input() data: any;
 
-  constructor(private keycloakService: KeycloakService, private cic: ClothingImageConverter) {
-  }
+  showAddButton: boolean = true;
+
+  constructor(
+    private keycloakService: KeycloakService,
+    private cic: ClothingImageConverter
+  ) {}
 
   isImageAtIndex(num: number): string {
     const pieces: { [key: string]: any }[] = this.data.pieces;
     if (pieces[num] && pieces[num]['image']) {
       return this.cic.base64ToImage(pieces[num]['image']);
     } else {
-      return this.cic.base64ToImage("iVBORw0KGgoAAAANSUhEUgAAAfQAAAF3CAYAAABT8rn8AAAC7klEQVR4Xu3BMQEAAADCoPVPbQsvoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACuBnPMAAHXcKOOAAAAAElFTkSuQmCC");
+      return this.cic.base64ToImage(
+        'iVBORw0KGgoAAAANSUhEUgAAAfQAAAF3CAYAAABT8rn8AAAC7klEQVR4Xu3BMQEAAADCoPVPbQsvoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACuBnPMAAHXcKOOAAAAAElFTkSuQmCC'
+      );
     }
   }
 
@@ -36,7 +40,11 @@ export class InspirationCardComponent {
     for (let i = 0; i < pieces.length; i++) {
       usages.push(pieces[i]['usage']);
     }
-    mostCommon = usages.reduce((a, b, i, arr) => arr.filter(v => v === a).length > arr.filter(v => v === b).length ? a : b);
+    mostCommon = usages.reduce((a, b, i, arr) =>
+      arr.filter((v) => v === a).length > arr.filter((v) => v === b).length
+        ? a
+        : b
+    );
     return mostCommon;
   }
 
@@ -47,7 +55,11 @@ export class InspirationCardComponent {
     for (let i = 0; i < pieces.length; i++) {
       seasons.push(pieces[i]['season']);
     }
-    mostCommon = seasons.reduce((a, b, i, arr) => arr.filter(v => v === a).length > arr.filter(v => v === b).length ? a : b);
+    mostCommon = seasons.reduce((a, b, i, arr) =>
+      arr.filter((v) => v === a).length > arr.filter((v) => v === b).length
+        ? a
+        : b
+    );
     return mostCommon;
   }
 
@@ -58,48 +70,62 @@ export class InspirationCardComponent {
     for (let i = 0; i < pieces.length; i++) {
       seasons.push(pieces[i]['color']);
     }
-    mostCommon = seasons.reduce((a, b, i, arr) => arr.filter(v => v === a).length > arr.filter(v => v === b).length ? a : b);
+    mostCommon = seasons.reduce((a, b, i, arr) =>
+      arr.filter((v) => v === a).length > arr.filter((v) => v === b).length
+        ? a
+        : b
+    );
     return mostCommon;
   }
 
   async rerollOutfit(usage: String): Promise<void> {
-    const apiUrl = `http://localhost:8080/api/v1/user/outfit/generate?user-id=${this.keycloakService.getKeycloakInstance().tokenParsed?.sub}&usage=${usage}`;
+    const apiUrl = `http://localhost:8080/api/v1/user/outfit/generate?user-id=${
+      this.keycloakService.getKeycloakInstance().tokenParsed?.sub
+    }&usage=${usage}`;
     const response = await fetch(apiUrl, {
       method: 'GET',
       headers: {
-        'Authorization': `Bearer ${await this.keycloakService.getToken()}`,
-        'Content-Type': 'application/json'
-      }
+        Authorization: `Bearer ${await this.keycloakService.getToken()}`,
+        'Content-Type': 'application/json',
+      },
     });
 
     if (response.ok) {
       this.data = await response.json();
+      this.showAddButton = true;
     } else {
-      window.alert("Fehlermeldung: Es konnten keine Outfits generiert werden. Bitte laden sie die Seite neu oder fügen sie neue Kleidungsstücke in ihren Kleiderschrank hinzu.");
+      window.alert(
+        'Fehlermeldung: Es konnten keine Outfits generiert werden. Bitte laden sie die Seite neu oder fügen sie neue Kleidungsstücke in ihren Kleiderschrank hinzu.'
+      );
       console.log(response.status);
     }
   }
 
   async addOutfit(outfit: any) {
-    const apiUrl = `http://localhost:8080/api/v1/user/outfit?user-id=${this.keycloakService.getKeycloakInstance().tokenParsed?.sub}`;
+    const apiUrl = `http://localhost:8080/api/v1/user/outfit?user-id=${
+      this.keycloakService.getKeycloakInstance().tokenParsed?.sub
+    }`;
     const requestBody = {
-      pieces: [outfit.pieces[0]._id, outfit.pieces[1]._id, outfit.pieces[2]._id],
-      isFavorite: false
+      pieces: [
+        outfit.pieces[0]._id,
+        outfit.pieces[1]._id,
+        outfit.pieces[2]._id,
+      ],
+      isFavorite: false,
     };
     const response = await fetch(apiUrl, {
       method: 'POST',
       headers: {
-        'Authorization': `Bearer ${await this.keycloakService.getToken()}`,
-        'Content-Type': 'application/json'
+        Authorization: `Bearer ${await this.keycloakService.getToken()}`,
+        'Content-Type': 'application/json',
       },
-      body: JSON.stringify(requestBody)
+      body: JSON.stringify(requestBody),
     });
 
     if (response.ok) {
-      console.log("Outfit added")
+      this.showAddButton = false;
     } else {
       console.log(response.status);
     }
   }
-
 }
