acd870705ee470c5c972039d0c8a54aa5d4e8380
topgggggg <156314616+topgggggg@users.noreply.github.com>
2024-06-06 00:13:40 +0200

    MSP-69 init clothing item interfaces

---
+++ src/app/interfaces/clothing.interface.ts
@@ -0,0 +1,26 @@
+export interface GetClothingItem {
+    _id: string;
+    name: string;
+    image: string;
+    brand: string;
+    color: string;
+    masterCategory: string;
+    subCategory: string;
+    type: string;
+    season: string;
+    usage: string;
+    isFavorite: boolean;
+}
+
+export interface AddClothingItem {
+    name: string;
+    image: string;
+    brand: string;
+    color: string;
+    masterCategory: string;
+    subCategory: string;
+    type: string;
+    season: string;
+    usage: string;
+    isFavorite: boolean;
+}
