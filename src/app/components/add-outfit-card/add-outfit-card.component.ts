ae20fe253b2fda6c557ecdb369b1a8bd76d07b6e
topgggggg <156314616+topgggggg@users.noreply.github.com>
2024-06-06 00:14:26 +0200

    MSP-69 init add outfit card

---
+++ src/app/components/add-outfit-card/add-outfit-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-outfit-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-outfit-card.component.html',
+  styleUrl: './add-outfit-card.component.css'
+})
+export class AddOutfitCardComponent { }
